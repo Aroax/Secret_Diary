1888d29bded742fc96cd22f5470853f9ab427fd2
Arran Oakes <arran.oakes@gmail.com>
2021-06-08 12:46:43 +0100

    encapsulated single class into three classes and added password requirement

--- lib/secretdiary.rb
+++ lib/secretdiary.rb
@@ -1,31 +1,77 @@
 class SecretDiary
-  attr_reader :locked
+  attr_reader :security
 
-  def initialize(locked=true)
-    @locked = locked
+  def initialize
+    @security = Diary_Security.new
+    @diary = []
   end
 
-  def add_entry
+  def add_entry(content)
     fail 'Diary locked: cannot add entry' if locked?
+    @diary << Diary_Entry.new(content)
   end
 
   def get_entry
     fail 'Diary locked: cannot get entry' if locked?
+    @diary.each do |entry|
+      entry.display
+    end
   end
 
-  def unlock
+  def unlock(password)
     fail 'Cannot unlock: diary already unlocked' if !locked?
-    @locked = false
+    @security.unlock!(password)
   end
 
-  def lock
+  def lock(password)
     fail 'Cannot lock: diary already locked' if locked?
-    @locked = true
+    @security.lock!(password)
   end
-  
-  private
 
+  private
   def locked?
-    @locked == true
+    @security.locked == true
   end
+end
+
+# Diary_Entry is a component of SecretDiary ("composition")
+class Diary_Entry
+  attr_reader :date, :content
+
+  def initialize(content="")
+    @content = content
+    @date = Time.new
+  end
+
+  def display
+    puts @date
+    puts @content
+  end
+end
+
+class Diary_Security
+    attr_reader :locked
+
+    def initialize(lock=true)
+      @locked = lock
+    end
+
+    def lock!(password)
+      fail "wrong password!" if !password?(password)
+      @locked = true
+    end
+
+    def unlock!(password)
+      fail "wrong password!" if !password?(password)
+      @locked = false
+    end
+
+    private
+
+    PASSWORD = "ruby"
+
+    def password?(password)
+      password == PASSWORD
+    end
+
 end
--- spec/secretdiary_spec.rb
+++ spec/secretdiary_spec.rb
@@ -1,16 +1,20 @@
 require 'secretdiary'
 describe SecretDiary do
+  let(:diary) { SecretDiary.new }
+  let(:content) { "Hello, world!" }
+  let(:password) {"ruby"}
+
   it 'is a diary object' do
     expect(SecretDiary).to respond_to(:new)
   end
 
   it 'starts locked' do
-    expect(subject.locked).to equal(true)
+    expect(diary.security.locked).to equal(true)
   end
 
   context 'when locked' do
     it 'raises an error when trying to add an entry' do
-      expect{ subject.add_entry }.to raise_error 'Diary locked: cannot add entry'
+      expect{ subject.add_entry(content) }.to raise_error 'Diary locked: cannot add entry'
     end
 
     it 'raises an error when trying to get an entry' do
@@ -18,21 +22,28 @@ describe SecretDiary do
     end
 
     it 'can be unlocked' do
-      subject.unlock
-      expect(subject.locked).to eq(false)
+      diary.unlock(password)
+      expect(diary.security.locked).to eq(false)
     end
 
     it 'raises an error if you try to lock' do
-      expect{ subject.lock }.to raise_error 'Cannot lock: diary already locked'
+      expect{ subject.lock(password) }.to raise_error 'Cannot lock: diary already locked'
     end
 
+    it 'raises an error if you do not supply the correct password when locking/unlocking' do
+      expect{ subject.unlock("apple") }.to raise_error "wrong password!"
+    end
   end
 
   context 'when unlocked' do
     it 'raises an error if you try to unlock' do
-      subject.unlock
-      expect{ subject.unlock }.to raise_error 'Cannot unlock: diary already unlocked'
+      subject.unlock(password)
+      expect{ subject.unlock(password) }.to raise_error 'Cannot unlock: diary already unlocked'
     end
+
+    # it 'can add an entry' do
+    #
+    # end
   end
 
 end
